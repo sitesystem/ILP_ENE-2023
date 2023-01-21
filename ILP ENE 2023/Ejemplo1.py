f74bc903724864287e98d2f6dcb266305869b068
sitesystem <l_aloaxa05@hotmail.com>
2023-01-21 11:20:13 -0600

    Ejemplo 1

---
+++ ILP ENE 2023/Ejemplo1.py
@@ -0,0 +1,17 @@
+# EJEMPLO 1. SALUDO
+# COMENTARIOS: Sirven para detallar y especificar cada parte del código
+# A los comentario los omite o no los toma en cuenta Python
+'''
+COMENTARIOS
+YJYUYUYU
+YUYUYU
+JJHJHJHJH
+'''
+
+# Imprimir en pantalla o mostrar resultados
+print(5+5)  # Suma
+print(10-2) # Resta
+print("Hola Mundo")
+print('Eduardo');
+
+print(7*8)
